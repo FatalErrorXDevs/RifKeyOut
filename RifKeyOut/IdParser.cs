363a70e39131e0330dca9f30b4196bc0aaee2bb4
Tom Prins <tom.prins2@hva.nl>
2017-09-19 07:47:01 +0200

    first version

---
+++ RifKeyOut/IdParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace RifKeyOut
+{
+    class IdParser
+    {
+        public void Parse(string path)
+        {
+            try
+            {
+            BinaryReader reader = new BinaryReader(new FileStream(path, FileMode.Open));
+            //Set the position of the reader
+            reader.BaseStream.Position = 0x10;
+            //Read the offset
+            String outPut = System.Text.Encoding.ASCII.GetString((reader.ReadBytes(36)));
+            String titleId = outPut.Substring(7, 9);
+            Console.WriteLine("Title ID : \t" + titleId); //reader.ReadBytes(32)
+            Console.WriteLine("Full pkg ID : \t" + outPut);
+
+            reader.BaseStream.Position = 0x50;
+            //Read the offset
+            String titleKey = BitConverter.ToString(reader.ReadBytes(16)).Replace("-", null);
+            String outPutFile = AppDomain.CurrentDomain.BaseDirectory + "\\" + titleId + ".txt";
+            Console.WriteLine("Title key : \t" + titleKey); //reader.ReadBytes(16)
+            File.WriteAllText(outPutFile, titleKey);
+                }catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+                throw;
+            }
+        }
+    }
+}
---
+++ RifKeyOut/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RifKeyOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IdParser parse = new IdParser();
+            if (args.Length != 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    
+                    String path = args[i];
+                    if (HasRifExtension(path))
+                    {
+                        parse.Parse(path);
+                    }
+                    else
+                    {
+                        Console.WriteLine("only rif files and work.bin are supported!");
+                    }
+
+
+                }
+
+            }
+            Console.WriteLine("Usage:");
+            Console.WriteLine("Drag rif files onto executable, output is in same location as the executable! ");
+            Console.Write("Press Enter to close window ...");
+            Console.Read();
+        }
+        public static bool HasRifExtension(string source)
+        {
+            return (source.EndsWith(".rif") || source.EndsWith(".bin"));
+        }
+    }
+}
